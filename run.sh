d20757648c1bb674a93f8669546058dc2ccdc95e
Samuel Frunza-Hincu <frunza_samuel@yahoo.com>
2026-03-22 17:16:45 +0100

    updated env var

--- run.sh
+++ run.sh
@@ -3,6 +3,5 @@
 # Exit immediately if a simple command exits with a nonzero exit value
 set -e
 
-docker build --build-arg SSH_PRIVATE_KEY="$SSH_PRIVATE_KEY" -t ansiblerke2 .
-#docker build --build-arg SSH_PRIVATE_KEY="$TARGET_MACHINE_SSH_PRIVATE_KEY" -t ansiblerke2 .
+docker build --build-arg SSH_PRIVATE_KEY="$TARGET_MACHINE_SSH_PRIVATE_KEY" -t ansiblerke2 .
 docker compose -f dockerCompose.yml run --rm main
